e752530b83d69e74edd1c6d8dbbad9b9269b17d2
SuhasPK <sparrowjack0682@gmail.com>
2023-08-13 12:42:54 +0530

    added dark theme to plot, added download image button, and published in Rstudio shiny.io site.

--- Diamonds_ShinyApp/server.R
+++ Diamonds_ShinyApp/server.R
@@ -7,22 +7,42 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
+library(shiny)
+library(ggplot2)
+library(ggdark)
 
-    output$distPlot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+function(input, output) {
+  
+  # Reload the data based on Sample Size selected by user
+  dataset <- reactive({
+    diamonds[sample(nrow(diamonds), input$sampleSize),]
+  })
+  
+  # Plot based on user inputs
+  output$plot <- renderPlot({
+    
+    p <- ggplot(dataset(), aes_string(x = input$x, y = input$y)) + geom_point()
+    
+    if (input$color != 'None')
+      p <- p + aes_string(color = input$color)
+    
+    facets <- paste(input$facet_row, '~', input$facet_col)
+    if (facets != '. ~ .')
+      p <- p + facet_grid(facets)
+    
+    p <- p + ggtitle(input$caption)+ dark_theme_linedraw() 
+    
+    print(p)
+    
+    output$downloadImage <- downloadHandler(
+      filename = function(){paste("download_plot", '.png', sep='')},
+      content = function(file){ggsave(file,plot = p)}
+    )
+    
+  }, height=800)
+  
 }
--- Diamonds_ShinyApp/ui.R
+++ Diamonds_ShinyApp/ui.R
@@ -8,26 +8,76 @@
 #
 
 library(shiny)
+library(shinythemes)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
-fluidPage(
+dataset <- diamonds
 
-    # Application title
-    titlePanel("Diamonds"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+pageWithSidebar(
+  
+  # Header panel with application name.
+  headerPanel("Diamond Data Analysis"),
+  sidebarPanel(
+    # Explaining the data attributes.
+    
+    h4('The variables are as follows:'),
+    h6('1. price : diamond price in USD'),
+    h6('2.    carat   : diamond weight'),
+    h6('3.    cut     : quality of the cut'),
+    h6('4.    color   : diamond color (J-Worst to D-Best)'),
+    h6('5.    clarity : how clear diamond is'),
+    h6('6.    x       : length in mm'),
+    h6('7.    y       : width in mm'),
+    h6('8.    z       : depth in mm'),
+    h6('9.    depth   : depth percentage'),
+    h6('10.    table   : width of top of diamond'),
+    br(),
+    
+    # Some form of user input for plotting
+    h4(' Select different parameters:'),
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset), value=min(5000, nrow(dataset)), step=100, round=0),
+    selectInput('x', 'X Axis Measure', names(dataset)),
+    selectInput('y', 'Y Axis Measure', names(dataset), names(dataset)[[7]]),
+    selectInput('color', 'Measure Color', c('None', names(dataset)), names(dataset)[[4]]),
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset)), names(dataset)[[2]]),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset))),
+    textInput('caption', 'Plot Caption', value='Plot on Diamond Dataset'),
+    downloadButton('downloadImage', 'Download modified image')),
+  
+  mainPanel(
+    # Add text to guide users of this application
+    h4('Introduction & How to use'),
+    p("A dataset containing the prices and other attributes of almost 54,000 Diamonds. Using this shiny application we can interactively change different plot attributes and App will plot those. This is very easy and interactive application which gives an idea of Diamond attributes and relation between them."),
+    br(),
+    
+    # call plot function
+    plotOutput('plot')
+  )
+  
+
+  
+  
+  
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
